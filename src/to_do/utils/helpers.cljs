626d9ba0f8cb52802095513fc7200b27f3b64058
Konstantin Yakushin <y231966mdk@gmail.com>
2016-09-06 12:06:59 +0700

    Simple function for finding index.

--- src/to_do/utils/helpers.cljs
+++ src/to_do/utils/helpers.cljs
@@ -1,2 +1,9 @@
 (ns to-do.utils.helpers)
 
+(defn find-index
+  "to find index in collection using the condition given.
+  If condition returns true, it returns index of the first element in collection.
+  pred - function with condition
+  coll - collection"
+  [pred coll]
+  (count (take-while pred coll)))
